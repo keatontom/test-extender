cba2d5e4dafbe107e2f78700cb803a0de16ecf37
keatontom <20kt34@queens.ca>
2024-05-21 15:33:10 -0400

    new file:   .eslintrc.json 	new file:   .gitignore 	new file:   .vscode-test.mjs 	new file:   .vscode/extensions.json 	new file:   .vscode/launch.json 	new file:   .vscode/settings.json 	new file:   .vscode/tasks.json 	new file:   .vscodeignore 	new file:   CHANGELOG.md 	new file:   README.md 	new file:   package-lock.json 	new file:   package.json 	new file:   src/extension.ts 	new file:   src/test/extension.test.ts 	new file:   tsconfig.json 	new file:   vsc-extension-quickstart.md

---
+++ src/extension.ts
@@ -0,0 +1,19 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+    const bonjourCommand = 'test-extender.bonjour';
+
+    context.subscriptions.push(vscode.commands.registerCommand(bonjourCommand, () => {
+        vscode.window.showInformationMessage('Bonjour from test-extender!');
+    }));
+
+    let api = {
+        bonjour() {
+            return vscode.commands.executeCommand(bonjourCommand);
+        }
+    };
+
+    return api;
+}
+
+export function deactivate() {}
